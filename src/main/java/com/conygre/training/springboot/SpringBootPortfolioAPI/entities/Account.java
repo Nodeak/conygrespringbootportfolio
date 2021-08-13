dc3872cc62ad7ab2a4882e976ae9dad694236f4a
AlexisAguila <aa17g@my.fsu.edu>
2021-08-13 12:50:41 -0400

    Added entitie classes, not implemented yet

---
+++ src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/Account.java
@@ -0,0 +1,8 @@
+package com.conygre.training.springboot.SpringBootPortfolioAPI.entities;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="Account")
+public class Account {
+}
---
+++ src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/Holdings.java
@@ -0,0 +1,8 @@
+package com.conygre.training.springboot.SpringBootPortfolioAPI.entities;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="Holdings")
+public class Holdings {
+}
