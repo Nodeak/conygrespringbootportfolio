5971ea6949d2a20e6f32bf061f41f595adbb1341
Nodeak <k2.futbol@gmail.com>
2021-08-13 11:27:58 -0400

    Create new Spring Boot project

---
+++ src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/SpringBootPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.conygre.training.springboot.SpringBootPortfolioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPortfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPortfolioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conygre/training/springboot/SpringBootPortfolioAPI/SpringBootPortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conygre.training.springboot.SpringBootPortfolioAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
