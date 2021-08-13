7e0b4e673634948babe9153afcb781faa5b45f6f
snrampersad <serena.rampersad56@gmail.com>
2021-08-13 17:20:15 +0000

    Mapped entities User, Holdings to DB layout. Included getters and setters.

--- src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/User.java
+++ src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/User.java
@@ -5,4 +5,50 @@ import java.io.Serializable;
 @Entity
 @Table(name="User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "net_worth")
+    private float net_worth;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public float getNet_worth() {
+        return net_worth;
+    }
+
+    public void setNet_worth(float net_worth) {
+        this.net_worth = net_worth;
+    }
+
 }
