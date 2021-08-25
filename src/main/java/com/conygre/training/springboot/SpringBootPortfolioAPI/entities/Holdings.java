4b8666d5b7bd994be927b34951676d3ae98dc2b1
Serena Rampersad <serena.rampersad56@gmail.com>
2021-08-25 15:12:29 +0000

    added new columns to holdings

--- src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/Holdings.java
+++ src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/Holdings.java
@@ -1,6 +1,7 @@
 package com.conygre.training.springboot.SpringBootPortfolioAPI.entities;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Date;
 
 @Entity
 @Table(name="Holdings")
@@ -25,20 +26,50 @@ public class Holdings implements Serializable {
     @Column(name = "buy_price")
     private float buyPrice;
 
+    @Column(name = "cur_price")
+    private float curPrice;
+
+    @Column(name = "buy_date")
+    private Date buyDate;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public float getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(float buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public float getCurPrice() {
+        return curPrice;
+    }
+
+    public void setCurPrice(float curPrice) {
+        this.curPrice = curPrice;
+    }
+
+    public Date getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(Date buyDate) {
+        this.buyDate = buyDate;
+    }
+
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public int getAccount_id() {
-        return accountId;
-    }
-
-    public void setAccount_id(int accountId) {
-        this.accountId = accountId;
     }
 
     public String getType() {
