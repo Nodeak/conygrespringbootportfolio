89628afa1b75651ef0ef7af39d40cc45e2579dee
Nodeak <k2.futbol@gmail.com>
2021-08-25 16:09:02 -0400

    Add amount property to Holdings entity

--- src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/Holdings.java
+++ src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/Holdings.java
@@ -10,11 +10,9 @@ public class Holdings implements Serializable {
     @Id
     @Column(name = "id")
     private int id;
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -22,44 +20,36 @@ public class Holdings implements Serializable {
 
     @Column(name = "account_id")
     private int accountId;
-
     public int getAccountId() {
         return accountId;
     }
-
     public void setAccountId(int accountId) {
         this.accountId = accountId;
     }
 
     @Column(name = "type")
     private String type;
-
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
 
     @Column(name = "name")
     private String name;
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
 
     @Column(name = "symbol")
     private String symbol;
-
     public String getSymbol() {
         return symbol;
     }
-
     public void setSymbol(String symbol) {
         this.symbol = symbol;
     }
@@ -67,33 +57,36 @@ public class Holdings implements Serializable {
 
     @Column(name = "buy_price")
     private float buyPrice;
-
     public float getBuyPrice() {
         return buyPrice;
     }
-
     public void setBuyPrice(float buyPrice) {
         this.buyPrice = buyPrice;
     }
 
+    @Column(name = "amount")
+    private int amount;
+    public float getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
     @Column(name = "cur_price")
     private float curPrice;
-
     public float getCurPrice() {
         return curPrice;
     }
-
     public void setCurPrice(float curPrice) {
         this.curPrice = curPrice;
     }
 
     @Column(name = "buy_date")
     private Date buyDate;
-
     public Date getBuyDate() {
         return buyDate;
     }
-
     public void setBuyDate(Date buyDate) {
         this.buyDate = buyDate;
     }
@@ -101,11 +94,9 @@ public class Holdings implements Serializable {
 
     @Column(name = "percent_change")
     private float percentChange;
-
     public float getPercentChange() {
         return percentChange;
     }
-
     public void setPercentChange(float percentChange) {
         this.percentChange = percentChange;
     }
--- src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/User.java
+++ src/main/java/com/conygre/training/springboot/SpringBootPortfolioAPI/entities/User.java
@@ -10,10 +10,37 @@ public class User implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id") private int id;
-    @Column(name = "first_name") private String firstName;
-    @Column(name = "last_name") private String lastName;
-    @Column(name = "net_worth") private float netWorth;
+    @Column(name = "id")
+    private int id;
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    @Column(name = "first_name")
+    private String firstName;
+    public String getFirst_name() {
+        return firstName;
+    }
+    public void setFirst_name(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    private String lastName;
+    public String getLast_name() {
+        return lastName;
+    }
+    public void setLast_name(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "net_worth")
+    private float netWorth;
+    public float getNet_worth() {
+        return netWorth;
+    }
+    public void setNet_worth(float netWorth) {
+        this.netWorth = netWorth;
+    }
 
     public User(){}
 
@@ -23,37 +50,13 @@ public class User implements Serializable{
         netWorth = nw;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
-    public String getFirst_name() {
-        return firstName;
-    }
 
-    public void setFirst_name(String firstName) {
-        this.firstName = firstName;
-    }
 
-    public String getLast_name() {
-        return lastName;
-    }
 
-    public void setLast_name(String lastName) {
-        this.lastName = lastName;
-    }
 
-    public float getNet_worth() {
-        return netWorth;
-    }
 
-    public void setNet_worth(float netWorth) {
-        this.netWorth = netWorth;
-    }
 
 
 }
